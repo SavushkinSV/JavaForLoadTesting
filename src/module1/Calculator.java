cf692b0b49efcf0fa5be01d8056779393eba7c8c
Savushkin Sergey <savushkinsv@mail.ru>
2025-04-11 10:46:49 +0300

    feat: add Calculator

---
+++ src/module1/Calculator.java
@@ -0,0 +1,63 @@
+package module1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Калькулятор принимает на вход два числа и знак операции (+, -, *, /),
+ * если был подан другой знак выводится ошибка.
+ */
+
+public class Calculator {
+    public static void main(String[] args) {
+        double result;
+        try {
+            double number1 = getNumber();
+            double number2 = getNumber();
+            char operation = getOperation();
+            result = calculate(number1, number2, operation);
+            System.out.println("Результат операции: " + result);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private static double calculate(double number1, double number2, char operation) throws IOException {
+        double result;
+        switch (operation) {
+            case '+' -> result = number1 + number2;
+            case '-' -> result = number1 - number2;
+            case '*' -> result = number1 * number2;
+            case '/' -> result = number1 / number2;
+            default -> throw new IOException("Ошибка. Некорректная операция.");
+        }
+
+        return result;
+    }
+
+    private static char getOperation() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите операцию над числами -> ");
+        char operation = 0;
+        if (sc.hasNext()) {
+            operation = sc.next().charAt(0);
+        }
+        sc.close();
+
+        return operation;
+    }
+
+    static double getNumber() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число -> ");
+        double number;
+        if (sc.hasNextDouble()) {
+            number = sc.nextDouble();
+        } else {
+            throw new IOException("Ошибка. Некорректное число");
+        }
+        return number;
+    }
+
+}
